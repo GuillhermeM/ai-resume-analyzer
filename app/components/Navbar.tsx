1edd9e953a0c37256a29fca8131e58fb7fb2dafd
Guillherme Mota <guillherme.mota@ufpe.br>
2025-08-22 21:55:14 -0300

    git commit -m "✨ feat: Add upload button to Navbar"

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,8 +6,9 @@ const Navbar = () => {
         <Link to="/">
             <p className="text-2xl font-bold text-gradient">RESUMIND</p>
         </Link>
-
-
+        <Link to="/upload" className="primary-button w-fit">
+          Upload Resume
+        </Link>
     </nav>
 
   )
