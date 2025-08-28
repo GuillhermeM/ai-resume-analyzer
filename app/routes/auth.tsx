c8762da12a53b13d4a3b08f093d270ce787171ff
Guillherme Mota <guillherme.mota@ufpe.br>
2025-08-28 18:45:20 -0300

    ✨ feat: Implement authentication page UI

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { usePuterStore } from "~/lib/puter"
+
+export const meta = () => ([ 
+    { title: 'Resumind | Auth' },
+    {name: 'description', content: 'log into your account'},
+])
+
 
 const Auth: () => Element = () => {
+    const { isLoading } = usePuterStore();
+
   return (
-    <div>
-        Auth
-    </div>
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <div className="gradient-border shadow-lg">
+            <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                <div className="flex flex-col items-center gap-2 text-center">
+                    <h1>Welcome</h1>
+                    <h2>Log in to Continue Your Job Journey</h2>
+                </div>
+
+            </section>
+
+        </div>
+    </main>
   )
 }
 
