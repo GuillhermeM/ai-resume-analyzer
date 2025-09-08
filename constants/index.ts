567e6951862f9a2a878199ec73cbd8daeca955f5
Guillherme Mota <guillherme.mota@ufpe.br>
2025-09-08 18:50:53 -0300

    ✨ feat: Added new mock resume data

--- constants/index.ts
+++ constants/index.ts
@@ -1,94 +1,184 @@
 export const resumes: Resume[] = [
-  {
-    id: "1",
-    companyName: "Google",
-    jobTitle: "Frontend Developer",
-    imagePath: "/images/resume_01.png",
-    resumePath: "/resumes/resume-1.pdf",
-    feedback: {
-      overallScore: 85,
-      ATS: {
-        score: 90,
-        tips: [],
-      },
-      toneAndStyle: {
-        score: 90,
-        tips: [],
-      },
-      content: {
-        score: 90,
-        tips: [],
-      },
-      structure: {
-        score: 90,
-        tips: [],
-      },
-      skills: {
-        score: 90,
-        tips: [],
-      },
+    {
+        id: "1",
+        companyName: "Google",
+        jobTitle: "Frontend Developer",
+        imagePath: "/images/resume_01.png",
+        resumePath: "/resumes/resume-1.pdf",
+        feedback: {
+            overallScore: 85,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
     },
-  },
-  {
-    id: "2",
-    companyName: "Microsoft",
-    jobTitle: "Cloud Engineer",
-    imagePath: "/images/resume_02.png",
-    resumePath: "/resumes/resume-2.pdf",
-    feedback: {
-      overallScore: 55,
-      ATS: {
-        score: 90,
-        tips: [],
-      },
-      toneAndStyle: {
-        score: 90,
-        tips: [],
-      },
-      content: {
-        score: 90,
-        tips: [],
-      },
-      structure: {
-        score: 90,
-        tips: [],
-      },
-      skills: {
-        score: 90,
-        tips: [],
-      },
+    {
+        id: "2",
+        companyName: "Microsoft",
+        jobTitle: "Cloud Engineer",
+        imagePath: "/images/resume_02.png",
+        resumePath: "/resumes/resume-2.pdf",
+        feedback: {
+            overallScore: 55,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
     },
-  },
-  {
-    id: "3",
-    companyName: "Apple",
-    jobTitle: "iOS Developer",
-    imagePath: "/images/resume_03.png",
-    resumePath: "/resumes/resume-3.pdf",
-    feedback: {
-      overallScore: 75,
-      ATS: {
-        score: 90,
-        tips: [],
-      },
-      toneAndStyle: {
-        score: 90,
-        tips: [],
-      },
-      content: {
-        score: 90,
-        tips: [],
-      },
-      structure: {
-        score: 90,
-        tips: [],
-      },
-      skills: {
-        score: 90,
-        tips: [],
-      },
+    {
+        id: "3",
+        companyName: "Apple",
+        jobTitle: "iOS Developer",
+        imagePath: "/images/resume_03.png",
+        resumePath: "/resumes/resume-3.pdf",
+        feedback: {
+            overallScore: 75,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
     },
-  },
+    {
+        id: "4",
+        companyName: "Google",
+        jobTitle: "Frontend Developer",
+        imagePath: "/images/resume_01.png",
+        resumePath: "/resumes/resume-1.pdf",
+        feedback: {
+            overallScore: 85,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
+    },
+    {
+        id: "5",
+        companyName: "Microsoft",
+        jobTitle: "Cloud Engineer",
+        imagePath: "/images/resume_02.png",
+        resumePath: "/resumes/resume-2.pdf",
+        feedback: {
+            overallScore: 55,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
+    },
+    {
+        id: "6",
+        companyName: "Apple",
+        jobTitle: "iOS Developer",
+        imagePath: "/images/resume_03.png",
+        resumePath: "/resumes/resume-3.pdf",
+        feedback: {
+            overallScore: 75,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
+    },
 ];
 
 export const AIResponseFormat = `
@@ -149,4 +239,3 @@ export const prepareInstructions = ({jobTitle, jobDescription}: { jobTitle: stri
       ${AIResponseFormat}
       Return the analysis as an JSON object, without any other text and without the backticks.
       Do not include any other text or comments.`;
-
